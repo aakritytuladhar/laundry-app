13cbe773ff2055bdc9ea34a9759b5b71283efb4b
aakritytuladhar <tuladhar.aakriti@gmail.com>
2024-01-16 10:01:15 +0545

    nav

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,82 +1,107 @@
 // rnfes
-import { StyleSheet, Text, View, SafeAreaView, Alert } from "react-native";
 import React, { useEffect, useState } from "react";
+import { View, Text, Pressable, Image, TextInput } from "react-native";
 import * as Location from "expo-location";
+import axios from "axios";
+// import { StyleSheet } from "react-native-web";
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
-  const [displayCurrentAddress, setdisplayCurrentAddress] = useState(
-    "we are loading your Location"
-  );
-  const [locationServiceEnabled, setlocationServiceEnabled] = useState(false);
-  useEffect(() => {
-    checkIfLocationEnabled();
-    getCurrentLocation();
-  }, []);
-  const checkIfLocationEnabled = async () => {
-    // checking wheather the location is on or off
-    let enabled = await Location.hasServicesEnabledAsync();
-    if (!enabled) {
-      Alert.alert(
-        "Location Sevice is not enabled",
-        "Please enable the location services",
-        [
-          {
-            text: "Cancel",
-            onPress: () => console.log("Cancel Pressed"),
-            style: "cancel",
-          },
-          { text: "OK", onPress: () => console.log("OK Pressed") },
-        ],
-        { cancelable: false }
-      );
-    } else {
-      setlocationServiceEnabledlocationServiceEnabled(enabled);
-    }
-  };
-  const getCurrentLocation = async () => {
-    // permission to enable the location
+  const [location, setLocation] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  const getLocationAsync = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
     if (status !== "granted") {
-      Alert.alert(
-        "Permission denied",
-        "Allow the app to use the Location services",
-        [
-          {
-            text: "Cancel",
-            onPress: () => console.log("Cancel Pressed"),
-            style: "cancel",
-          },
-          { text: "OK", onPress: () => console.log("OK Pressed") },
-        ],
-        { cancelable: false }
-      );
+      console.error("Location permission not granted");
+      return;
     }
-    // provide latitude and longitude of the current location
-    const { coords } = await Location.getCurrentPositionAsync();
-    console.log(coords);
-    if (coords) {
-      // destructuring
-      const { latitude, longitude } = coords;
-      // reverse fun is used to get exact location
-      let response = await Location.reverseGeocodeAsync({
-        latitude,
-        longitude,
-      });
-      console.log(response);
-      for (let item of response) {
-        let address = `${item.name} ${item.city} ${item.postalCode}`;
-        setdisplayCurrentAddress(address);
-        console.log(address);
-      }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+
+    // Get address from coordinates using OpenCage Geocoding API
+    try {
+      const response = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${location.coords.latitude}+${location.coords.longitude}&key=2590a55760894b02831b3a7f62541516`
+      );
+      setAddress(response.data.results[0].formatted);
+    } catch (error) {
+      console.error("Error fetching address:", error);
     }
   };
+
+  useEffect(() => {
+    getLocationAsync();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>{displayCurrentAddress}</Text>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        {/* location and profile */}
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            padding: 10,
+          }}>
+          <MaterialIcons name="location-on" size={30} color="red" />
+          <View>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "600",
+                position: "relative",
+                zIndex: 1,
+              }}>
+              Home
+            </Text>
+            {location && (
+              <Text style={{ paddingRight: 72 }}>
+                {address && <Text>{address}</Text>}
+              </Text>
+            )}
+          </View>
+          <Pressable style={{ marginLeft: "auto", marginRight: 7 }}>
+            <Image
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                position: "relative",
+                zIndex: 1,
+                float: "left",
+              }}
+              source={{
+                uri: "https://lh3.googleusercontent.com/-RnpzrGr1z-w/AAAAAAAAAAI/AAAAAAAAAAA/AFNEGgL8i7Pea4iiU-eW2pLtFSAapYEdcg/photo.jpg?sz=46",
+              }}
+            />
+          </Pressable>
+        </View>
+        {/* Search Bar */}
+        <View
+          style={{
+            padding: 10,
+            margin: 10,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            borderWidth: 0.8,
+            borderColor: "638889",
+            borderRadius: 7,
+            height: 40,
+          }}>
+          <TextInput placeholder="Search for item or more" />
+          <AntDesign name="search1" size={20} color="red" />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+// const styles = StyleSheet.create({
+
+// });
